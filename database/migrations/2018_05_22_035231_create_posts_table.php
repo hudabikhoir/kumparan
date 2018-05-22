09fe8b403e8efb45db9c96d4cd3a73a1b733ecea
hudaparodi <hudaparodi@gmail.com>
2018-05-22 11:00:38 +0700

    init project

---
+++ database/migrations/2018_05_22_035231_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('news_id');
+            $table->unsignedInteger('topic_id');
+            $table->enum('status', ['draft', 'deleted', 'publish']);
+            $table->timestamps();
+
+            $table->foreign('news_id')->references('id')->on('news');
+            $table->foreign('topic_id')->references('id')->on('topics');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
